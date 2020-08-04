7fb40891c5f1ccc4a9e061030c944e165977668d
unknown <support@novatel.com>
2020-08-04 15:27:57 -0600

    Update

--- lua/TEMPLATE_PROJECT/make_my_project.bat
+++ lua/TEMPLATE_PROJECT/make_my_project.bat
@@ -3,6 +3,10 @@
 :: NovAtel Inc. 
 :: This is a make.bat wrapper to produce a .hex file from a sample OEM7 API script
 ::---------------------------------------------------------------------------------
+::
+:: To relocate this script, edit as follows:
+:: pushd must be followed by relative path to where the make.bat file is
+:: make.bat must be followed by relative path from where make.bat is, to the directory with your OEM7 API .lua script files
 
 pushd ..\..\
 make.bat lua\TEMPLATE_PROJECT\lua
